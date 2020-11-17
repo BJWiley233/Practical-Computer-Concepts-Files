1a7a9e7daaac86208efd132ee4a12ecab983d9be
BJWiley23 <bwiley4@jh.edu>
2020-11-17 03:27:39 -0500

    Add files via upload

--- linux_shell/variant/var_pipe.sh
+++ linux_shell/variant/var_pipe.sh
@@ -2,12 +2,17 @@
 
 ### run like 
 	#chmod +x var_pipe.sh
-	#./var_pipe.sh srr.ids &
-	# Requires installing TRIMMOMATIC, freebayes, vcflib as adding vcflib binaries folder to your $PATH
+	#./var_pipe.sh srr.ids refGene_BRCA1_exons_hg19_transcript1.bed
+if [ $# -lt 2 ]; then
+    echo "Need srr.ids and refGene_exons.bed files"
+    exit 1
+fi
 
-# IMPORTANT !!!!! Update THIS to you Trimmomatic jar path and freebayes binary !!!!!
+
+# !!!!! Update THIS to you Trimmomatic jar path and freebayes binary !!!!!
 export TRIMMOMATIC=$TOOLS/Trimmomatic-0.39/trimmomatic-0.39.jar;
-export FREEBAYES=$TOOLS/freebayes-v1.3.0-1;
+export FREEBAYES=$TOOLS/freebayes/bin/freebayes;
+#export PATH=$PATH:/path/to/vcflib/bin:/path/to/bedtools/bin:/path/to/samtools/bin
 
 # get reference for freebayes and indexes from bowtie2
 wget https://genome-idx.s3.amazonaws.com/bt/hg19.zip
@@ -17,10 +22,10 @@ gunzip hg19.fa.gz
 samtools faidx hg19.fa
 
 # get reads from SRA
-#parallel -a $1 fastq-dump --split-files --origfmt --gzip;
+parallel -a $1 fastq-dump --split-files --origfmt --gzip;
 
 # run qc on all downloads
-#fastqc *.fastq.gz
+fastqc *.fastq.gz
 
 # read in srr.ids file
 readarray -t mySRRs < $1
@@ -34,18 +39,32 @@ for id in "${mySRRs[@]}"; do
 
 	fastqc ${id}_*.trim.fastq.gz
 	
+	
+	
 	bowtie2 -x hg19 -1 ${id}_1.trim.fastq.gz -2 ${id}_2.trim.fastq.gz --threads 32 --fast -S ${id}_paired.hg19.trim.sam;
+	
 	samtools sort ${id}_paired.hg19.trim.sam -o ${id}_paired.hg19.trim.sorted.bam -O bam;
+	
 	samtools index ${id}_paired.hg19.trim.sorted.bam;
 
+	
+	# could make this an input 
+	#if [ $# -eq 3 ]; then
+	#	-r $3  # for $3="chr17:0-81195210"
+	#fi
 	$FREEBAYES -f hg19.fa ${id}_paired.hg19.trim.sorted.bam -r chr17:0-81195210 > ${id}_paired.hg19.trim.vcf;
 
 	vcffilter -f "DP > 10 & AF = 0.5" ${id}_paired.hg19.trim.vcf > ${id}_paired.hg19.trim.filtered.vcf;
-	vcfannotate -b refGene_BRCA1_exons_hg19_transcript1.bed \
+	
+	vcfannotate -b $2 \
 		-k REFGENE \
 		${id}_paired.hg19.trim.filtered.vcf > ${id}_paired.hg19.trim.filtered.annot.vcf;
 		
+	
+	
 	echo ${id};
-	bedtools intersect -a ${id}_paired.hg19.trim.filtered.vcf -b refGene_BRCA1_exons_hg19_transcript1.bed | wc -l;
+	bedtools intersect -a ${id}_paired.hg19.trim.filtered.vcf -b $2 | wc -l;
 
 done;
+
+
